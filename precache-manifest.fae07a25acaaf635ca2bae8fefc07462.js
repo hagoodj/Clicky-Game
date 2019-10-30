a19e2d5c392cdeee2c5ba571966486ba5b8639fe
jordan hagood <jordan.hagood@yahoo.com>
2019-10-30 14:55:51 -0700

    Updates

---
+++ precache-manifest.fae07a25acaaf635ca2bae8fefc07462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd3d4bee6b349cbc3b9066dd553561a",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "342d0269a3f19af17bab",
+    "url": "/Clicky-Game/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "4fb6c9bea8f558224c16",
+    "url": "/Clicky-Game/static/js/2.b7a6145f.chunk.js"
+  },
+  {
+    "revision": "342d0269a3f19af17bab",
+    "url": "/Clicky-Game/static/js/main.7cbe821f.chunk.js"
+  },
+  {
+    "revision": "43a7a25f6b13dd044116",
+    "url": "/Clicky-Game/static/js/runtime-main.de921e77.js"
+  }
+]);
